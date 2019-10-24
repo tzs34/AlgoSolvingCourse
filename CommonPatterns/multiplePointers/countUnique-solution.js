af83381bc4241f1efd35c11d03d082733e2d4b26
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-10-24 13:52:38 +0100

    findUnique example using pointer approach

---
+++ CommonPatterns/multiplePointers/countUnique-solution.js
@@ -0,0 +1,12 @@
+function countUnique (arr){
+    let i = 0
+
+    for(var j = 1 ; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i ++
+            arr[i] = arr[j]
+
+        }
+    } 
+    return arr.slice(0, i+1).length
+}
