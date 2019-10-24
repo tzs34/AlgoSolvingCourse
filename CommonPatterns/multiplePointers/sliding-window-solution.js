c9ca3ea4033e45206cbe8d6dad08d4fc285a6be1
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-10-24 14:38:03 +0100

    Sliding windows examples

---
+++ CommonPatterns/multiplePointers/sliding-window-solution.js
@@ -0,0 +1,16 @@
+/*
+
+The sliding window is good for finding a subset
+e.g. unique string of chars in a string
+
+Sliding window allows you to keep track of a subset of data within a larger set of data.
+
+Example 
+Function takes an array and a number
+Find the max sum of n consecutive digits in the array , if number is 2 then max sum of 2 digits in the array
+maxSubArraySum([1,2,5,2,8,1,5,], 2) // 10  2 + 8 = 10
+*/
+
+function maxSubArraySum(){
+
+}
